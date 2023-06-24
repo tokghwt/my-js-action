b21d27ac0caeb2faddfc3b7e08aff7f5b0500085
tokghwt <tokghwt@gmail.com>
2023-06-24 23:38:38 +0900

    feat: Change index.js (v0.4.0)

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ try {
   const pattern = core.getInput('pattern', { required: true, trimWhitespace: false});
   const flags = core.getInput('flags');
   const regexp = new RegExp(pattern, flags);
-  core.debug(`core.debug: regexp: ${regexp}`);
-  console.log('console.log: regexp:', regexp);
-  //const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log('console.log: github.context:', github.context);
-  for (const commit of github.context.payload.commits) {
-    console.log('console.log: commit message:', commit.message);
+  core.debug(`regexp: ${regexp}`);
+  if (github.context.eventName === 'push') {
+    for (const commit of github.context.payload.commits) {
+      core.debug('commit message:', commit.message);
+      if (!regexp.test(commit.message)) {
+        throw new Error(`Wrong format commit message (${commit.message})`);
+      }
+    }
+  } else {
+    console.log('github.context:', JSON.stringify(github.context, null, 2));
   }
 } catch (error) {
   core.setFailed(error.message);
