27fb543be9bafebcf4614663109e0903f0ebd8b7
itou0104jp <itou0104jp@gmail.com>
2023-06-26 17:21:36 +0900

    feat: Change index.js (v0.5.0)

--- dist/index.js
+++ dist/index.js
@@ -9796,16 +9796,21 @@ try {
   const pattern = core.getInput('pattern', { required: true, trimWhitespace: false});
   const flags = core.getInput('flags');
   const regexp = new RegExp(pattern, flags);
-  core.debug(`regexp: ${regexp}`);
+  core.info(`regexp: ${String(regexp)}`);
   if (github.context.eventName === 'push') {
     for (const commit of github.context.payload.commits) {
-      core.debug('commit message:', commit.message);
+      core.debug(`commit message: "${commit.message}"`);
       if (!regexp.test(commit.message)) {
-        throw new Error(`Wrong format commit message (${commit.message})`);
+        throw new Error(`Incorrect format commit message ("${commit.message}")`);
       }
     }
+  } else if (github.context.eventName === 'pull_request') {
+    core.debug(`pull request title: "${github.context.payload.pull_request.title}"`);
+    if (!regexp.test(github.context.payload.pull_request.title)) {
+      throw new Error(`Incorrect format pull request title ("${github.context.payload.pull_request.title}")`);
+    }
   } else {
-    console.log('github.context:', JSON.stringify(github.context, null, 2));
+    core.info(`github.context: ${JSON.stringify(github.context, null, 2)}`);
   }
 } catch (error) {
   core.setFailed(error.message);
