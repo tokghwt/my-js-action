72207a7affd0d7828f718861ab5bdf2e0f24f348
itou0104jp <itou0104jp@gmail.com>
2023-06-27 17:51:41 +0900

    feat: Change index.js (v0.7.0)

--- dist/index.js
+++ dist/index.js
@@ -9799,8 +9799,8 @@ try {
   const regexp = new RegExp(pattern, flags);
   core.info(`input regexp: ${String(regexp)}`);
   core.info(`event name: ${github.context.eventName}`);
-  core.info(`activity type: ${github.context.payload.action}`);
   if (github.context.eventName === 'push') {
+    core.info(`git ref: ${github.context.payload.ref}`);
     for (const commit of github.context.payload.commits) {
       core.info(`commit message: "${commit.message}"`);
       if (!regexp.test(commit.message)) {
@@ -9808,6 +9808,7 @@ try {
       }
     }
   } else if (github.context.eventName === 'pull_request' || github.context.eventName === 'pull_request_target') {
+    core.info(`activity type: ${github.context.payload.action}`);
     core.info(`pull request title: "${github.context.payload.pull_request.title}"`);
     if (!regexp.test(github.context.payload.pull_request.title)) {
       throw new Error(`Incorrect format pull request title ("${github.context.payload.pull_request.title}")`);
--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ try {
   const regexp = new RegExp(pattern, flags);
   core.info(`input regexp: ${String(regexp)}`);
   core.info(`event name: ${github.context.eventName}`);
-  core.info(`activity type: ${github.context.payload.action}`);
   if (github.context.eventName === 'push') {
+    core.info(`git ref: ${github.context.payload.ref}`);
     for (const commit of github.context.payload.commits) {
       core.info(`commit message: "${commit.message}"`);
       if (!regexp.test(commit.message)) {
@@ -17,6 +17,7 @@ try {
       }
     }
   } else if (github.context.eventName === 'pull_request' || github.context.eventName === 'pull_request_target') {
+    core.info(`activity type: ${github.context.payload.action}`);
     core.info(`pull request title: "${github.context.payload.pull_request.title}"`);
     if (!regexp.test(github.context.payload.pull_request.title)) {
       throw new Error(`Incorrect format pull request title ("${github.context.payload.pull_request.title}")`);
