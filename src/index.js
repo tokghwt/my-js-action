8a6828811f379ee01b6a9bddd100000b1d57307d
itou0104jp <itou0104jp@gmail.com>
2023-06-27 22:55:28 +0900

    feat: Change index.js (v0.8.0)

--- src/index.js
+++ src/index.js
@@ -3,22 +3,14 @@ const github = require('@actions/github');
 
 try {
   core.debug(`github.context: ${JSON.stringify(github.context, null, 2)}`);
-  const pattern = core.getInput('pattern', { required: true, trimWhitespace: false});
-  const flags = core.getInput('flags');
-  const regexp = new RegExp(pattern, flags);
-  core.info(`input regexp: ${String(regexp)}`);
   core.info(`event name: ${github.context.eventName}`);
-  if (github.context.eventName === 'push') {
-    core.info(`git ref: ${github.context.payload.ref}`);
-    for (const commit of github.context.payload.commits) {
-      core.info(`commit message: "${commit.message}"`);
-      if (!regexp.test(commit.message)) {
-        throw new Error(`Incorrect format commit message ("${commit.message}")`);
-      }
-    }
-  } else if (github.context.eventName === 'pull_request' || github.context.eventName === 'pull_request_target') {
+  if (github.context.eventName === 'pull_request') {
     core.info(`activity type: ${github.context.payload.action}`);
     core.info(`pull request title: "${github.context.payload.pull_request.title}"`);
+    const pattern = core.getInput('pattern', { required: true, trimWhitespace: false});
+    const flags = core.getInput('flags');
+    const regexp = new RegExp(pattern, flags);
+    core.info(`input regexp: ${String(regexp)}`);
     if (!regexp.test(github.context.payload.pull_request.title)) {
       throw new Error(`Incorrect format pull request title ("${github.context.payload.pull_request.title}")`);
     }
